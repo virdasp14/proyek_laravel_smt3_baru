8b9ff2a8e7f572df5a7d5fc880e47f9cb83ddaac
virdasp14 <virdasp14@gmail.com>
2025-10-05 06:53:19 +0700

    CRUD tabel employee

--- proyek_laravel_smt3_baru/routes/web.php
+++ proyek_laravel_smt3_baru/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\EmployeeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::resource('employees',EmployeeController::class);
